4c1b23110729116cc5e40152550b79da76564790
Tristan Wise <tristan@tristanwise.com>
2021-01-13 00:21:02 -0500

    add created planets

--- hex-generator/src/index.js
+++ hex-generator/src/index.js
@@ -3,51 +3,46 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import hexTiles from "./hex-tiles";
 
-console.log(hexTiles);
-
 ReactDOM.render(
   <div className="App">
     {hexTiles.reduce((accum, tile, index) => {
       const count = tile.count || 1;
       for (let i = 0; i < count; i++) {
+        const randomColor =
+          "#" + Math.floor(Math.random() * 16777215).toString(16);
         accum.push(
           <span className="hex" key={`${index} + ${i}`}>
             <div className="hex-inner-wrapper">
-              <div className="hex-inner-text">{tile.text || ""}</div>
+              <div className="hex-inner-text">{/*tile.text || ""*/}</div>
+              {/*This renders the planet*/}
               <div
-                className="hex-center"
-                style={{ backgroundColor: tile.color }}
+                className="hex-planet"
+                style={{ backgroundColor: randomColor /*tile.color*/ }}
               ></div>
 
-              {
-                //Create a ring at random at about the clip we see in our solr system.
-                tile.color && Math.random() > 0.9
-                  ? [...Array(parseInt(Math.random() * 1.1) + 1)].map(
-                      (e, i) => {
-                        const ringHeight = Math.random() * 0.5 + 0.5;
-                        const ringWidth = ringHeight * Math.random() * 2;
+              {Math.random() > 1 - 0.2
+                ? [...Array(parseInt(Math.random() * (1 + 0.1)) + 1)].map(
+                    (e, i) => {
+                      const ringHeight = Math.random() + 0.5;
+                      const ringWidth = 0.33 * Math.random();
+                      const rotation = parseInt(Math.random() * 360);
 
-                        const rotation = parseInt(Math.random() * 360);
-
-                        console.log(ringHeight, ringWidth, rotation);
-
-                        return (
-                          <div
-                            className="hex-ring"
-                            style={{
-                              borderColor: tile.color,
-                              height: ringHeight.toString() + "in",
-                              width: ringWidth.toString() + "in",
-                              transform: `rotate(${
-                                rotation.toString() + "deg"
-                              })`,
-                            }}
-                          ></div>
-                        );
-                      },
-                    )
-                  : null
-              }
+                      return (
+                        <div
+                          className="hex-ring"
+                          style={{
+                            border: randomColor
+                              ? "1px solid " + randomColor
+                              : null,
+                            height: ringHeight.toString() + "in",
+                            width: ringWidth.toString() + "in",
+                            transform: `rotate(${rotation.toString() + "deg"})`,
+                          }}
+                        ></div>
+                      );
+                    },
+                  )
+                : null}
             </div>
           </span>,
         );
