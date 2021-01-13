3fb197f3306a1da0a3120e6dbbc24f82408ec0c0
Tristan Wise <tristan@tristanwise.com>
2021-01-12 23:55:12 -0500

    add random interval for rings

--- hex-generator/src/hex-tiles.js
+++ hex-generator/src/hex-tiles.js
@@ -1,9 +1,57 @@
 const hexTiles = [
   {
-    count: 10,
+    name: "empty space",
+    count: 50,
   },
   {
-    name: "test",
+    text: "Adamantine:3",
+    color: "khaki",
+    count: 3,
+  },
+  {
+    text: "Amalgamate:3",
+    color: "DarkSlateBlue",
+    count: 3,
+  },
+  {
+    text: "Black-Matter:3",
+    color: "darkgrey",
+    count: 3,
+  },
+  {
+    text: "Anti-Matter:3",
+    color: "darkred",
+    count: 3,
+  },
+  {
+    text: "Adamantine:1 Amalgamate:1",
+    color: "green",
+    count: 3,
+  },
+  {
+    text: "Adamantine:1 Anti-Matter:1",
+    color: "darkkhaki",
+    count: 3,
+  },
+  {
+    text: "Adamantine:1 Black-Matter:1",
+    color: "olive",
+    count: 3,
+  },
+  {
+    text: "Amalgamate:1 Black-Matter:1",
+    color: "darkblue",
+    count: 3,
+  },
+  {
+    text: "Amalgamate:1 Anti-Matter:1",
+    color: "purple",
+    count: 3,
+  },
+  {
+    text: "Black-Matter:1 Anti-Matter:1",
+    color: "maroon",
+    count: 3,
   },
 ];
 
--- hex-generator/src/index.js
+++ hex-generator/src/index.js
@@ -11,8 +11,44 @@ ReactDOM.render(
       const count = tile.count || 1;
       for (let i = 0; i < count; i++) {
         accum.push(
-          <span className="hex" key={index + i}>
-            <div className="hex-inner">{tile.name || ""}</div>
+          <span className="hex" key={`${index} + ${i}`}>
+            <div className="hex-inner-wrapper">
+              <div className="hex-inner-text">{tile.text || ""}</div>
+              <div
+                className="hex-center"
+                style={{ backgroundColor: tile.color }}
+              ></div>
+
+              {
+                //Create a ring at random at about the clip we see in our solr system.
+                tile.color && Math.random() > 0.9
+                  ? [...Array(parseInt(Math.random() * 1.1) + 1)].map(
+                      (e, i) => {
+                        const ringHeight = Math.random() * 0.5 + 0.5;
+                        const ringWidth = ringHeight * Math.random() * 2;
+
+                        const rotation = parseInt(Math.random() * 360);
+
+                        console.log(ringHeight, ringWidth, rotation);
+
+                        return (
+                          <div
+                            className="hex-ring"
+                            style={{
+                              borderColor: tile.color,
+                              height: ringHeight.toString() + "in",
+                              width: ringWidth.toString() + "in",
+                              transform: `rotate(${
+                                rotation.toString() + "deg"
+                              })`,
+                            }}
+                          ></div>
+                        );
+                      },
+                    )
+                  : null
+              }
+            </div>
           </span>,
         );
       }
